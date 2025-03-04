838ae524e59691f322eab1908852df93b839e298
Jomanh S Alqahtani <442408932@nu.edu.sa>
2025-03-04 11:00:19 +0300

    Promotion Req edits, changes in public file, review request file management

---
+++ JavaScripts/Review-Request.js
@@ -0,0 +1,78 @@
+const steps = document.querySelectorAll(".step");
+const formSteps = document.querySelectorAll(".form-step");
+const nextBtns = document.querySelectorAll(".next");
+const backBtns = document.querySelectorAll(".back");
+const confirmBtn = document.querySelector(".confirm");
+
+let currentStep = 0;
+
+function updateStepProgress() {
+  steps.forEach((step, index) => {
+    if (index < currentStep) {
+      step.classList.remove("preactive");
+      step.classList.add("active");
+    } else if (index === currentStep) {
+      step.classList.add("preactive");
+      step.classList.remove("active");
+    } else {
+      step.classList.remove("preactive", "active");
+    }
+  });
+}
+
+function showStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep);
+  });
+  updateStepProgress();
+}
+
+nextBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentStep < formSteps.length - 1) {
+      currentStep++;
+      showStep();
+    }
+  });
+});
+
+backBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentStep > 0) {
+      currentStep--;
+      showStep();
+    }
+  });
+});
+
+confirmBtn.addEventListener("click", () => {
+  alert("Request Completed!");
+});
+
+function displayFileName() {
+  const fileInput = document.getElementById("files");
+  const fileName = fileInput.files[0]
+    ? fileInput.files[0].name
+    : "No file selected";
+  document.getElementById("file-name").textContent = fileName;
+}
+
+function profileCard() {
+  var userId = document.getElementById("userId").value;
+  var optionsDiv = document.getElementById("user");
+
+  if (userId === "123") {
+    // هذا مثال مبدئي، راح يختلف بعد ما ينربط بالداتابيس
+    fetch("usercontainer.html") // تحميل ملف HTML الجانبي
+      .then((response) => response.text()) // تحويله إلى نص
+      .then((data) => {
+        optionsDiv.innerHTML = data; // عرض المحتوى داخل div
+        optionsDiv.classList.add("active");
+      })
+      .catch((error) => console.error("Can not upload", error));
+  } else {
+    optionsDiv.innerHTML = "<p>No options available for this ID</p>";
+  }
+}
+
+showStep();
